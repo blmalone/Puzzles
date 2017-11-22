95f9034c3c8230bf9eeb84f2d81554726a2bc88f
BlaineMalone <bmalone05@qub.ac.uk>
2017-11-22 17:55:57 +0000

    update reverse string algor

--- java_puzzles/src/com/company/ReverseString.java
+++ java_puzzles/src/com/company/ReverseString.java
@@ -2,7 +2,7 @@ package com.company;
 
 public class ReverseString {
 
-    public static void main(String[] args) {
+    public static void main( String[] args ) {
         String name = "blaine";
 
         /*
@@ -12,6 +12,7 @@ public class ReverseString {
         for(int i = name.length() -1; i >= 0; i--) {
             res = res + name.charAt(i);
         }
+
         System.out.println("Name: " + name + " Reverse Name: " + res + " -> ITERATIVE");
 
         /*
@@ -19,6 +20,7 @@ public class ReverseString {
          */
         res = recursiveReverseString(name);
         System.out.println("Name: " + name + " Reverse Name: " + res + " -> RECURSIVE");
+        System.out.println();
 
     }
 
@@ -26,7 +28,7 @@ public class ReverseString {
         if (str.length() == 1) {
             return str;
         } else {
-            return str.substring(str.length()-1, str.length()-1) + recursiveReverseString(str.substring(1));
+            return recursiveReverseString(str.substring(1)) + str.substring(0,1);
         }
     }
 }
